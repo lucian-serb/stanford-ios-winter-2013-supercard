e08b2bfcd95bf251d59835d06357fb46c91e8ca5
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-10-10 16:48:16 +0300

    Add methods for drawing pips

--- SuperCard/SuperCard/PlayingCardView.m
+++ SuperCard/SuperCard/PlayingCardView.m
@@ -45,6 +45,80 @@
     return self;
 }
 
+#define PIP_FONT_SCALE_FACTOR 0.2
+#define PIP_HOFFSET_PERCERTANGE 0.165
+#define PIP_VOFFSET1_PERCENTAGE 0.263
+#define PIP_VOFFSET2_PERCENTAGE PIP_VOFFSET1_PERCENTAGE / 2
+#define PIP_VOFFSET3_PERCENTAGE PIP_VOFFSET1_PERCENTAGE / 3
+#define PIP_VOFFSET4_PERCENTAGE PIP_VOFFSET3_PERCENTAGE * 2
+
+- (void)drawPips
+{
+    if ((self.rank == 1) || (self.rank == 3) || (self.rank == 5) || (self.rank == 9)) {
+        [self drawPipsWithHorizontalOffset:0.0 verticalOffset:0.0 mirroredVerticaly:NO];
+    }
+    
+    if ((self.rank == 6) || (self.rank == 7) || (self.rank == 8)) {
+        [self drawPipsWithHorizontalOffset:PIP_HOFFSET_PERCERTANGE verticalOffset:0.0 mirroredVerticaly:NO];
+    }
+    
+    if ((self.rank == 2) || (self.rank == 3)) {
+        [self drawPipsWithHorizontalOffset:0.0 verticalOffset:PIP_VOFFSET1_PERCENTAGE mirroredVerticaly:YES];
+    }
+    
+    if ((self.rank == 4) || (self.rank == 5) || (self.rank == 6) || (self.rank == 7) || (self.rank == 8) || (self.rank ==9) || (self.rank == 10)) {
+        [self drawPipsWithHorizontalOffset:PIP_HOFFSET_PERCERTANGE verticalOffset:PIP_VOFFSET1_PERCENTAGE mirroredVerticaly:YES];
+    }
+    
+    if ((self.rank == 7) || (self.rank == 8)) {
+        [self drawPipsWithHorizontalOffset:0.0 verticalOffset:PIP_VOFFSET2_PERCENTAGE mirroredVerticaly:(self.rank != 7)];
+    }
+    
+    if ((self.rank == 9) || (self.rank == 10)) {
+        [self drawPipsWithHorizontalOffset:PIP_HOFFSET_PERCERTANGE verticalOffset:PIP_VOFFSET3_PERCENTAGE mirroredVerticaly:YES];
+    }
+    
+    if (self.rank == 10) {
+        [self drawPipsWithHorizontalOffset:0.0 verticalOffset:PIP_VOFFSET4_PERCENTAGE mirroredVerticaly:YES];
+    }
+}
+
+- (void)drawPipsWithHorizontalOffset:(CGFloat)hoffset
+                      verticalOffset:(CGFloat)voffset
+                   mirroredVerticaly:(BOOL)mirroredVerticaly
+{
+    [self drawPipsWithHorizontalOffset:hoffset verticalOffset:voffset upsideDown:NO];
+    
+    if (mirroredVerticaly) {
+        [self drawPipsWithHorizontalOffset:hoffset verticalOffset:voffset upsideDown:YES];
+    }
+}
+
+- (void)drawPipsWithHorizontalOffset:(CGFloat)hoffset
+                      verticalOffset:(CGFloat)voffset
+                          upsideDown:(BOOL)upsideDown
+{
+    if (upsideDown) {
+        [self saveContextAndRotateUpsideDown];
+    }
+    
+    CGPoint middle = CGPointMake(self.bounds.size.width / 2.0, self.bounds.size.height / 2.0);
+    UIFont *pipFont = [UIFont systemFontOfSize:self.bounds.size.width * (self.rank == 1 ? PIP_FONT_SCALE_FACTOR * 2 : PIP_FONT_SCALE_FACTOR)];
+    NSAttributedString *attributedSuit = [[NSAttributedString alloc] initWithString:self.suit attributes:@{NSFontAttributeName: pipFont}];
+    CGSize pipSize = [attributedSuit size];
+    CGPoint pipOrigin = CGPointMake(middle.x - pipSize.width / 2.0 - hoffset * self.bounds.size.width, middle.y - pipSize.height / 2.0 - voffset * self.bounds.size.height);
+    [attributedSuit drawAtPoint:pipOrigin];
+    
+    if (hoffset) {
+        pipOrigin.x += hoffset * 2.0 * self.bounds.size.width;
+        [attributedSuit drawAtPoint:pipOrigin];
+    }
+    
+    if (upsideDown) {
+        [self restoreContext];
+    }
+}
+
 - (void)drawRect:(CGRect)rect
 {
     UIBezierPath *roundedRect = [UIBezierPath bezierPathWithRoundedRect:self.bounds cornerRadius:12.0];
